6f4e39d2ac51210912a55847f47b84789e6612b7
hanh lockett <honghanhbr2007@yahoo.com>
2023-11-27 14:26:18 +1100

    login

---
+++ db/migrate/20231127032043_add_column_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_number, :integer
+    add_column :users, :admin, :boolean
+    add_column :users, :company, :boolean
+  end
+
+end
