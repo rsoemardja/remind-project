13f62e9f332f1d42b5672df379f29196662cb664
hanh lockett <honghanhbr2007@yahoo.com>
2023-11-28 22:34:14 +1100

    add routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,24 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  post '/notes/:note_id/notes/:id/show_confirm', to: "notes#confirm", as: "confirm"
+  post '/notes/:note_id/notes/:id/show_reject', to: "notes#reject", as: "reject"
+
+
   # root "posts#index"
   resources :users do
     resources :posts, only: %i[index new create]
   end
+  resources :notes do
+    resources :notes, only: [:new, :create, :show]
+  end
+  resources :notes, only: [:destroy, :index]
+
+  
 end
