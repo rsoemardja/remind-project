518ba48d11d99fa7af741a72ea883150099c0a04
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2023-11-28 15:48:04 +1100

    clean

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,95 +13,44 @@ class NotesController < ApplicationController
 
   def create
     @note = Note.new(note_params)
+    @note.user = current_user
     if @note.save
-      redirect_to @note
+      redirect_to notes_path(@note)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
-<<<<<<< HEAD
-
-    def show
-        set_note
-    end
-
-    def new
-        @note = Note.new
-    end
-
-    def create
-        @note = Note.new(note_params)
-        @note.user = current_user
-        if @note.save
-            redirect_to notes_path(@note)
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
-
-    def edit
-        set_note
-    end
-
-    def update
-        # set_note
-        # if @note.update(note_params)
-        #     redirect_to @note
-        # else
-        #     render :edit
-        # end
-        set_note
-        @note.update(note_params)
-        redirect_to notes_path(@note)
-    end
-
-    def destroy
-        set_note
-        @note.destroy
-        redirect_to notes_path, status: :see_other
-    end
-
-    private
-
-    def set_note
-        @note = Note.find(params[:id])
-    end
-    
-    def note_params
-        params.require(:note).permit(:title, :description, :categories, :due_date )
-    end
-
-    def set_note
-        # Finds model per id
-        @note = Note.find(params[:id])
-      end
-=======
   end
 
   def edit
-    @note = Note.find(params[:id])
+    set_note
   end
 
   def update
-    if @note.update(note_params)
-      redirect_to @note
-    else
-      render :edit
-    end
+    # set_note
+    # if @note.update(note_params)
+    #     redirect_to @note
+    # else
+    #     render :edit
+    # end
+    set_note
+    @note.update(note_params)
+    redirect_to notes_path(@note)
   end
 
   def destroy
+    set_note
     @note.destroy
-    redirect_to notes_path
+    redirect_to notes_path, status: :see_other
   end
 
   private
 
-  def set_note
-    @note = Note.find(params[:id])
+  def note_params
+    params.require(:note).permit(:title, :description, :categories, :due_date)
   end
 
-  def note_params
-    params.require(:note).permit(:title, :description)
+  def set_note
+    # Finds model per id
+    @note = Note.find(params[:id])
   end
->>>>>>> 71fefb4e7454e5e547d43173d55bc7e2879190b8
 end
