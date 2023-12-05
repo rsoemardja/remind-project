5bf432fd72f3e73515dfb521fe41600d435070dd
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2023-12-05 18:00:33 +1100

    implement timer (with hacks) and model

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,5 +7,21 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :due_date, presence: true
 
+  def hours_remaining
+    hours_remaining = ((self.due_date - DateTime.now) / 3600) - 11
+    if hours_remaining > 0
+      format('%02d', hours_remaining)
+    else
+      format('%02d', 0)
+    end
+  end
 
+  def minutes_remaining
+    minutes_remaining = ((self.due_date - DateTime.now) % 3600 / 60)
+    if minutes_remaining > 0
+      format('%02d', minutes_remaining)
+    else
+      format('%02d', 0)
+    end
+  end
 end
