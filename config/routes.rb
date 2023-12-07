3980ad82bdef83a9861056b90ce47ea7bb8bec84
hanh lockett <honghanhbr2007@yahoo.com>
2023-12-08 10:41:29 +1100

    review

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :tasks, as: :tasks, except: :destroy
+  resources :tasks, as: :tasks, except: :destroy do
     resources :reviews, only: :create
+  end
     
 
   get "tasks/:id/destroy", to: "tasks#destroy", as: :destroy_task
